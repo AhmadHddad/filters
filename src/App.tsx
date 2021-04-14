4426ff0c2c086a2f49ec83e6f11d15aae94fe9b9
Ahmad Naalweh <ahmadhddad@outlook.com>
2021-04-14 21:24:32 +0300

    adding chips

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,39 @@
 import React from "react";
+import Header from "./components/Header/Header";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Logo from "./components/Logo/Logo";
+import Chip from "@material-ui/core/Chip";
+import attachmentData from "./attachmentData";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      flexGrow: 1,
+      display: "flex",
+      justifyContent: "start",
+      flexWrap: "wrap",
+      listStyle: "none",
+      padding: theme.spacing(0.5),
+      margin: 0,
+    },
+    chip: {
+      margin: theme.spacing(0.5),
     },
   })
 );
 
 function App() {
   const classes = useStyles();
-
+  const filtersKeys = Object.keys(attachmentData);
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <Logo />
-        </Toolbar>
-      </AppBar>
+    <div>
+      <Header />
+      <div>
+        <ul className={classes.root}>
+          {filtersKeys.map((key) => (
+            <Chip label={key} className={classes.chip} />
+          ))}
+          <li></li>
+        </ul>
+      </div>
     </div>
   );
 }
