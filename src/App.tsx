b2e6260640d5d250f5e3e6206a64e659282f1db2
Ahmad Naalweh <ahmadhddad@outlook.com>
2021-04-16 16:57:27 +0300

    relocating to body

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,12 @@
 import React from "react";
 import Header from "./components/Header/Header";
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import Chip from "@material-ui/core/Chip";
-import attachmentData from "./attachmentData";
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      display: "flex",
-      justifyContent: "start",
-      flexWrap: "wrap",
-      listStyle: "none",
-      padding: theme.spacing(0.5),
-      margin: 0,
-    },
-    chip: {
-      margin: theme.spacing(0.5),
-    },
-  })
-);
+import Body from "./containers/Body/Body";
 
 function App() {
-  const classes = useStyles();
-  const filtersKeys = Object.keys(attachmentData);
   return (
     <div>
       <Header />
-      <div>
-        <ul className={classes.root}>
-          {filtersKeys.map((key) => (
-            <Chip label={key} className={classes.chip} />
-          ))}
-          <li></li>
-        </ul>
-      </div>
+      <Body />
     </div>
   );
 }
