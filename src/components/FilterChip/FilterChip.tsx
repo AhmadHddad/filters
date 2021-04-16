aeeaef7036a2284e110e9f4bda962f868e98ad57
Ahmad Naalweh <ahmadhddad@outlook.com>
2021-04-16 18:05:56 +0300

    adding selected style

--- src/components/FilterChip/FilterChip.tsx
+++ src/components/FilterChip/FilterChip.tsx
@@ -1,18 +1,25 @@
-import * as React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Chip, { ChipProps } from "@material-ui/core/Chip";
-import FilterChipStyle from "./FilterChipStyle";
-import classNames from "clsx";
+import * as React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Chip, { ChipProps } from '@material-ui/core/Chip';
+import FilterChipStyle from './FilterChipStyle';
+import classNames from 'clsx';
 
 const useStyles = makeStyles(FilterChipStyle);
 
-export interface IFilterChipProps extends ChipProps {}
+export interface IFilterChipProps extends ChipProps {
+   selected?: boolean;
+}
 
 const FilterChip: React.FunctionComponent<IFilterChipProps> = (props) => {
-  const { className, ...rest } = props;
-  const classes = useStyles(props);
+   const { className, selected, ...rest } = props;
+   const classes = useStyles(props);
 
-  return <Chip className={classNames(classes.chip, className)} {...rest} />;
+   return (
+      <Chip
+         className={classNames(classes.chip, className, { [classes.selected]: selected })}
+         {...rest}
+      />
+   );
 };
 
 export default FilterChip;
--- src/components/FilterChip/FilterChipStyle.ts
+++ src/components/FilterChip/FilterChipStyle.ts
@@ -1,28 +1,33 @@
-import { createStyles, Theme } from "@material-ui/core/styles";
-import { IFilterChipProps } from "./FilterChip";
+import { createStyles, Theme } from '@material-ui/core/styles';
+import { IFilterChipProps } from './FilterChip';
+
+const hoverSelectedStyle = {
+   backgroundColor: 'cornflowerblue',
+   color: 'white',
+};
 
 const FilterChipStyle = (theme: Theme) =>
-  createStyles({
-    root: {
-      display: "flex",
-      justifyContent: "start",
-      flexWrap: "wrap",
-      listStyle: "none",
-      padding: theme.spacing(0.5),
-      boxShadow: "inset 0 0 6px rgba(0,0,0,0.3)",
-      margin: 0,
-    },
-    chip: (props: IFilterChipProps) => ({
-      margin: theme.spacing(0.5),
-      textTransform: "capitalize",
-      "&:hover": props.onClick
-        ? {
-            backgroundColor: "cornflowerblue",
-            color: "white",
-            cursor: "pointer",
-          }
-        : {},
-    }),
-  });
+   createStyles({
+      root: {
+         display: 'flex',
+         justifyContent: 'start',
+         flexWrap: 'wrap',
+         listStyle: 'none',
+         padding: theme.spacing(0.5),
+         boxShadow: 'inset 0 0 6px rgba(0,0,0,0.3)',
+         margin: 0,
+      },
+      chip: (props: IFilterChipProps) => ({
+         margin: theme.spacing(0.5),
+         textTransform: 'capitalize',
+         '&:hover': props.onClick
+            ? {
+                 ...hoverSelectedStyle,
+                 cursor: 'pointer',
+              }
+            : {},
+      }),
+      selected: hoverSelectedStyle,
+   });
 
 export default FilterChipStyle;
