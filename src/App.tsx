08cdbd1a0ba2600c5636ddf121ac1c01ed1c0ee5
Ahmad Naalweh <ahmadhddad@outlook.com>
2021-04-17 19:00:13 +0300

    adding styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import Header from "./components/Header/Header";
-import Body from "./containers/Body/Body";
+import React from 'react';
+import Header from './components/Header/Header';
+import Body from './containers/Body/Body';
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <Body />
-    </div>
-  );
+   return (
+      <div className={'fullWidthHeight'}>
+         <Header />
+         <Body />
+      </div>
+   );
 }
 
 export default App;
