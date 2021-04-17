8822042d998b6790a5780329b9412508ff014318
Ahmad Naalweh <ahmadhddad@outlook.com>
2021-04-17 21:59:14 +0300

    enhancing style

--- src/components/AppliedFilters/AppliedFilters.tsx
+++ src/components/AppliedFilters/AppliedFilters.tsx
@@ -2,17 +2,21 @@ import * as React from 'react';
 import AppliedFiltersList, {
    IAppliedFiltersListProps,
 } from './../AppliedFiltersList/AppliedFiltersList';
+import Grid from '@material-ui/core/Grid';
+import { Typography } from '@material-ui/core';
 
 export interface IAppliedFiltersProps extends IAppliedFiltersListProps {}
 
 export default function AppliedFilters(props: IAppliedFiltersProps) {
    const { ...rest } = props;
    return (
-      <div>
-         <span>Applied Filters:</span>
-         <span>
+      <Grid container spacing={1}>
+         <Grid item style={{ display: 'flex', alignItems: 'center' }}>
+            <Typography display="inline">Applied Filters:</Typography>
+         </Grid>
+         <Grid item>
             <AppliedFiltersList {...rest} />
-         </span>
-      </div>
+         </Grid>
+      </Grid>
    );
 }
