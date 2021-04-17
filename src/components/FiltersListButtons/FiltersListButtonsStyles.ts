5ce32be6620d221122a232f2fd85be34719a7cca
Ahmad Naalweh <ahmadhddad@outlook.com>
2021-04-17 16:58:54 +0300

    enhancing style

--- src/components/FiltersListButtons/FiltersListButtonsStyles.ts
+++ src/components/FiltersListButtons/FiltersListButtonsStyles.ts
@@ -3,7 +3,6 @@ import { createStyles, Theme } from '@material-ui/core/styles';
 const FiltersListButtons = (theme: Theme) =>
    createStyles({
       gridContainer: {
-         maxHeight: 240,
          overflow: 'auto',
          width: '100%',
          '&>div': {
