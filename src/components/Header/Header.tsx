a935674aa8058220c9d10f9119011be868e4e55c
Ahmad Naalweh <ahmadhddad@outlook.com>
2021-04-17 18:37:03 +0300

    passing props

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,18 @@
-import * as React from "react";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Logo from "../Logo/Logo";
+import * as React from 'react';
+import AppBar, { AppBarProps } from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Logo from '../Logo/Logo';
 
-interface IHeaderProps {}
+interface IHeaderProps extends AppBarProps {}
 
 const Header: React.FunctionComponent<IHeaderProps> = (props) => {
-  return (
-    <AppBar position="static">
-      <Toolbar>
-        <Logo />
-      </Toolbar>
-    </AppBar>
-  );
+   return (
+      <AppBar position="static" {...props}>
+         <Toolbar>
+            <Logo />
+         </Toolbar>
+      </AppBar>
+   );
 };
 
 export default Header;
