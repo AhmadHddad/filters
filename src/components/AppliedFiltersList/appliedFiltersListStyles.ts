bca6cd9d24ba6dcb167ffcabea08a44bdb633bf2
Ahmad Naalweh <ahmadhddad@outlook.com>
2021-04-17 21:59:38 +0300

    removing unessary style

--- src/components/AppliedFiltersList/appliedFiltersListStyles.ts
+++ src/components/AppliedFiltersList/appliedFiltersListStyles.ts
@@ -4,14 +4,10 @@ const appliedFiltersListStyles = (theme: Theme) =>
    createStyles({
       root: {
          display: 'flex',
-         justifyContent: 'center',
          flexWrap: 'wrap',
          listStyle: 'none',
          padding: theme.spacing(0.5),
          margin: 0,
-      },
-      chip: {
-         margin: theme.spacing(0.5),
       },
    });
 
