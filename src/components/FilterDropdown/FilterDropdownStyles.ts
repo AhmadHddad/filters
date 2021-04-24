4620a7c294965ff75cacf67d3111bd4cb0cdc3de
Ahmad Naalweh <ahmadhddad@outlook.com>
2021-04-24 17:54:22 +0300

    removing unused styles

--- src/components/FilterDropdown/FilterDropdownStyles.ts
+++ src/components/FilterDropdown/FilterDropdownStyles.ts
@@ -1,12 +1,7 @@
 import { createStyles, Theme } from '@material-ui/core/styles';
-import { IFilterDropdownProps } from './FilterDropdown';
 
 const filterDropdownStyles = (theme: Theme) =>
    createStyles({
-      paperContainer: (props: IFilterDropdownProps) => ({
-         width: props.isMobile ? '100%' : '300px',
-         height: '300px',
-      }),
       gridContainer: {
          width: '100%',
          height: '100%',
